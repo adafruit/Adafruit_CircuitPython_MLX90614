2b2db1f914ebaf915e6c0493c4834d429d6edf47
Mikey Sklar <mikesklar@gmail.com>
2018-06-26 21:39:13 -0600

    updated example to have correct store product links and removed read_ prefix on public functions

--- examples/mlx90614_simpletest.py
+++ examples/mlx90614_simpletest.py
@@ -1,7 +1,7 @@
 #  Designed specifically to work with the MLX90614 sensors in the
 #  adafruit shop
-#  ----> https://www.adafruit.com/products/1748
-#  ----> https://www.adafruit.com/products/1749
+#  ----> https://www.adafruit.com/product/1747
+#  ----> https://www.adafruit.com/product/1748
 #
 #  These sensors use I2C to communicate, 2 pins are required to
 #  interface Adafruit invests time and resources providing this open
@@ -13,17 +13,11 @@ import board
 import busio as io   
 import adafruit_mlx90614
 
-# use bitbangio only with ESP8266 
-# * does not support hardware i2c
-# * comment out busio above
-# * express boards can use also bitbangio, but they have i2c hardware built-in
-#import bitbangio as io 
-
 # the mlx90614 must be run at 100k [normal speed]
 # i2c default mode is is 400k [full speed] 
 # the mlx90614 will not appear at the default 400k speed
 i2c = io.I2C(board.SCL, board.SDA, frequency=100000) 
 mlx = adafruit_mlx90614.MLX90614(i2c)
 
-print("Ambent Temp: ", mlx.read_ambient_temp_f)
-print("Object Temp: ", mlx.read_object_temp_f)
+print("Ambent Temp: ", mlx.ambient_temp_f)
+print("Object Temp: ", mlx.object_temp_f)
