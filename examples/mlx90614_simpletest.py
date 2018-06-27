e3c215cc9882c354e9938fbbec9f9721bef95381
Mikey Sklar <mikesklar@gmail.com>
2018-06-27 14:50:17 -0600

    pylint cleaup, confirm working on hardware

--- examples/mlx90614_simpletest.py
+++ examples/mlx90614_simpletest.py
@@ -10,13 +10,13 @@
 #  products from Adafruit!
 
 import board
-import busio as io   
+import busio as io
 import adafruit_mlx90614
 
 # the mlx90614 must be run at 100k [normal speed]
-# i2c default mode is is 400k [full speed] 
+# i2c default mode is is 400k [full speed]
 # the mlx90614 will not appear at the default 400k speed
-i2c = io.I2C(board.SCL, board.SDA, frequency=100000) 
+i2c = io.I2C(board.SCL, board.SDA, frequency=100000)
 mlx = adafruit_mlx90614.MLX90614(i2c)
 
 print("Ambent Temp: ", mlx.ambient_temp_f)
