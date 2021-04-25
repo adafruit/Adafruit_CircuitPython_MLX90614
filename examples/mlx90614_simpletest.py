a7ceb6e6f6b30f7c3512a7f065892d3a7b1cb43c
jposada202020 <jquintana202020@gmail.com>
2021-04-25 10:48:35 -0400

    improving_docs

--- examples/mlx90614_simpletest.py
+++ examples/mlx90614_simpletest.py
@@ -13,13 +13,11 @@
 #  products from Adafruit!
 
 import board
-import busio as io
 import adafruit_mlx90614
 
 # the mlx90614 must be run at 100k [normal speed]
-# i2c default mode is is 400k [full speed]
-# the mlx90614 will not appear at the default 400k speed
-i2c = io.I2C(board.SCL, board.SDA, frequency=100000)
+# the mlx90614 will not appear at the 400k speed
+i2c = board.I2C()
 mlx = adafruit_mlx90614.MLX90614(i2c)
 
 # temperature results in celsius
